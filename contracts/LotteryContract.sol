0d316845e1d92bd18bd70d557777f60ea5184608
Ashwani <ashwani90125@gmail.com>
2025-02-15 00:22:36 +0530

    Add lottery based contract

---
+++ contracts/LotteryContract.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Lottery {
+    address public manager;
+    address[] public players;
+
+    constructor() {
+        manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value > 0.01 ether, "Minimum 0.01 ETH required");
+        players.push(msg.sender);
+    }
+
+    function random() private view returns (uint) {
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
+    }
+
+    function pickWinner() public restricted {
+        uint index = random() % players.length;
+        payable(players[index]).transfer(address(this).balance);
+        players = new address[](0); // Reset the lottery
+    }
+
+    modifier restricted() {
+        require(msg.sender == manager, "Only manager can call this function");
+        _;
+    }
+
+    function getPlayers() public view returns (address[] memory) {
+        return players;
+    }
+}
