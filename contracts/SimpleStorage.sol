de4e8972a6f6ee627132972229a061bd15c89452
Ashwani <ashwani90125@gmail.com>
2025-02-18 00:43:44 +0530

    Simple storage implementation

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.28;
+
+contract SimpleStorage {
+    uint storedData;
+    function set(uint x) public {
+        storedData = x;
+    }
+
+    function get() public view returns (uint) {
+        return storedData;
+    }
+}
